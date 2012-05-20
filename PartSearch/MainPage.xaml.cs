b81ad854758e83c191a3b3dae07c631696cc6a23
Mathias Rabe <mathias-rabe@post-x.de>
2012-05-20 16:04:13 +0200

    First Commit - nothing special until now

---
+++ PartSearch/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PartSearch
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Konstruktor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
