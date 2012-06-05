4847efeedafb383c6b5882da72d0b5ebaa616a4d
lesky <benjamin.lesky@gmx.de>
2012-06-05 12:27:55 +0300

    Update master

--- PartSearch/Parser/Buerklin.cs
+++ PartSearch/Parser/Buerklin.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Windows;
 using System.Collections.Generic;
 using PartSearch.Models;
+using HtmlAgilityPack;
+using System.ComponentModel;
 
 namespace PartSearch.Parser
 {
@@ -20,12 +22,35 @@ namespace PartSearch.Parser
 
         public override List<Product> GetParts()
         {
-            //TODO: Mit Leben füllen!
-            //siehe Klasse HtmlDocument
-            //HTML Text sollte in _htmlText stehen
-            List<Product> list = new List<Product>();
-            return list;
-        }
+             List<Product> list = new List<Product>();
+            // Hilfsvariablen begindn mit h
 
-    }
-}
+            string hname;
+            string hpreis;
+            ObservableCollection<Gericht> tmpGerichte = new ObservableCollection<Gericht>();
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(HTML);
+            var hname = doc.DocumentNode.SelectSingleNode("//td[@class='Typ']").InnerText;
+            var hpreis = doc.DocumentNode.SelectSingleNode("//td[@class='Brutto']").InnerText;
+
+
+            /* Vorlage München
+            var gerichtNummer = doc.DocumentNode.SelectSingleNode("//td[@class='gericht']").InnerText;
+            var gericht1 = doc.DocumentNode.SelectSingleNode("//td[@class='beschreibung']/span").InnerText;
+
+            var dates = doc.DocumentNode.SelectNodes("//table[@class='menu']/tr/td/span/a");
+            var date1 = dates[0].InnerText;
+
+            var menus = doc.DocumentNode.SelectNodes("//table[@class='menu']");
+
+            foreach (HtmlNode menu in menus)
+            {
+                HtmlDocument newDoc = new HtmlDocument();
+                newDoc.LoadHtml(menu.InnerHtml);
+                var names = newDoc.DocumentNode.SelectNodes("//td[@class='beschreibung']/span[@style='float:left']");
+
+                var date = newDoc.DocumentNode.SelectSingleNode("//tr/td/span/a").InnerText;
+
+                foreach (HtmlNode name in names)
+                {
