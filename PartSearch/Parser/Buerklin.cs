93d973f90d940737c4cc549c0a581466a6f5e9fc
lesky <benjamin.lesky@gmx.de>
2012-06-07 17:49:46 +0300

    Fehler korigiert //TODO:: //-Typcasting //-Eventuell Strings weiter Bearbeiten //-Übergabe an Modell

--- PartSearch/Parser/Buerklin.cs
+++ PartSearch/Parser/Buerklin.cs
@@ -25,15 +25,27 @@ namespace PartSearch.Parser
              List<Product> list = new List<Product>();
             // Hilfsvariablen begindn mit h
 
-            string hname;
-            string hpreis;
+            private string _name;
+            private double _price;
+            private int _quantity;
+
+            private string hname;
+            private string hprice;
+            private string hquantity
+
             ObservableCollection<Gericht> tmpGerichte = new ObservableCollection<Gericht>();
 
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(HTML);
+            // Auslesen Der Daten
             var hname = doc.DocumentNode.SelectSingleNode("//td[@class='Typ']").InnerText;
-            var hpreis = doc.DocumentNode.SelectSingleNode("//td[@class='Brutto']").InnerText;
+            var hprice = doc.DocumentNode.SelectSingleNode("//td[@class='Brutto']").InnerText;
+            var hquantity = doc.DocumentNode.SelectSingleNode("//td[@class='/*Hir Suchbegriff Eintragen*/']").InnerText;
 
+            //TODO::
+            //-Typcasting
+            //-Übergabe an Modell
+            //-Eventuell Strings weiter Bearbeiten
 
             /* Vorlage München
             var gerichtNummer = doc.DocumentNode.SelectSingleNode("//td[@class='gericht']").InnerText;
